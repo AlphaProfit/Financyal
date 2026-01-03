c36cce411a31922baefdbbb98a47526c15bcbfc4
AlphaProfit <aoskin0.24@gmail.com>
2026-01-04 00:48:25 +0800

    Update database.py

--- database.py
+++ database.py
@@ -1,45 +1,38 @@
-from sqlalchemy import BigInteger, String, Text, Integer, ForeignKey
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 import os
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import BigInteger, String, Text, ForeignKey, Integer
 
-# Используем SQLite для тестов, но на Railway заменим на POSTGRES_URL
-DB_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///db.sqlite3")
+# Для Koyeb будем использовать локальный файл базы данных
+DB_URL = "sqlite+aiosqlite:///chill_seria.db"
 
-engine = create_async_engine(DB_URL, echo=False)
+engine = create_async_engine(DB_URL)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 class Base(AsyncAttrs, DeclarativeBase):
     pass
 
-# Таблица фильмов
 class Movie(Base):
     __tablename__ = 'movies'
-    
     id: Mapped[int] = mapped_column(primary_key=True)
-    code: Mapped[int] = mapped_column(unique=True)  # Код фильма
+    code: Mapped[int] = mapped_column(unique=True)
     title: Mapped[str] = mapped_column(String(255))
     description: Mapped[str] = mapped_column(Text)
-    photo_id: Mapped[str] = mapped_column(String(255)) # file_id фото
+    photo_id: Mapped[str] = mapped_column(String(255))
     rating: Mapped[str] = mapped_column(String(50))
     link: Mapped[str] = mapped_column(String(500))
 
-# Таблица каналов для обязательной подписки
 class Channel(Base):
     __tablename__ = 'channels'
-    
     id: Mapped[int] = mapped_column(primary_key=True)
-    channel_id: Mapped[int] = mapped_column(BigInteger, unique=True) # ID канала (-100...)
-    url: Mapped[str] = mapped_column(String(255)) # Ссылка приглашение
+    channel_id: Mapped[int] = mapped_column(BigInteger, unique=True)
+    url: Mapped[str] = mapped_column(String(255))
 
-# Таблица лайков (Избранное)
 class Favorite(Base):
     __tablename__ = 'favorites'
-    
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(BigInteger)
     movie_id: Mapped[int] = mapped_column(ForeignKey('movies.id'))
-    
     movie: Mapped["Movie"] = relationship()
 
 async def init_db():
